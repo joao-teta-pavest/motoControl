5965e4302979246b3321b665261b12ff91119d54
joaoteta <joaoteta@192.168.0.199>
2017-12-27 17:25:41 +0100

    Primeiro Teste Git

---
+++ application/views/index.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+
+<html>
+<head>
+
+<title>Login</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet"
+	href="<?php echo base_url('assets/css/base.css');?>">
+<!-- jQuery library -->
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<link rel="stylesheet"
+	href=" <?php echo base_url('assets/css/base.css');?> ">
+<link rel="stylesheet"
+	href=" <?php echo base_url('assets/css/MudaImagem.css');?> ">
+</head>
+<body>
+<h1>Teste Git</h1>
+</body>
+</html>
