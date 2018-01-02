0b1e7a179dbdae2585ac36856828858da12e2c34
Lerby Ganga <Lerby Ganga@DESKTOP-SBMEUHC>
2018-01-02 16:27:09 +0100

    adicionado formulario

--- application/views/login.php
+++ application/views/login.php
@@ -24,7 +24,29 @@
 	href=" <?php echo base_url('assets/css/menu.css');?> ">
 </head>
 <body>
-
+<div>
+   
+            <div class="row" style="margin-top:10%">
+                <div class="col-md-4">
+                  
+                </div>
+                
+                
+                <div class="col-md-4 formContainer">
+            <form method="get" id="fmoto" name="fmoto" action="" style=" margin:0 auto;width:250px"> 
+                <fieldset id="Usuario"><h3>Login</h3> 
+                <br>
+                    <input type="text" name="tUser" id="tUser"   placeholder="User"><br><br>
+                    <input type="password" name="tPass" id="tPass" placeholder="Palavra passe"/><br><br>
+                    <input type="button" onclick="alert('Lerby Langa')" value="Entrar" class="btn btn-success" />
+            </fieldset> 
+                
+            </form>
+                    </div>
+            <div class="col-md-4"></div>
+            </div>
+      
+        </div>     
 	<a href=" <?php echo site_url('Index_ctrl')?>">Entrar</a>
 
 
